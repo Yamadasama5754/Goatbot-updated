9405aa66e956a12a153b1de5ab9cfd3f6dbfc150
Yamada_UX <160421051+Yamadasama5754@users.noreply.github.com>
2024-10-01 15:49:58 +0100

    Update luckranck.js

--- scripts/cmds/luckranck.js
+++ scripts/cmds/luckranck.js
@@ -12,31 +12,31 @@ module.exports = {
     category: "معلومات",
     guide: {
       vi: "   {pn} <إسم الأمر> <القاعدة الجديدة>: set role mới cho lệnh"
-        + "\n   Với:"
-        + "\n   + <commandName>: tên lệnh"
-        + "\n   + <new role>: role mới của lệnh với:"
-        + "\n   + <new role> = 0: lệnh có thể được sử dụng bởi mọi thành viên trong nhóm"
-        + "\n   + <new role> = 1: lệnh chỉ có thể được sử dụng bởi quản trị viên"
-        + "\n   + <new role> = default: reset role lệnh về mặc định"
-        + "\n   Ví dụ:"
-        + "\n    {pn} rank 1: (lệnh rank sẽ chỉ có thể được sử dụng bởi quản trị viên)"
-        + "\n    {pn} rank 0: (lệnh rank sẽ có thể được sử dụng bởi mọi thành viên trong nhóm)"
-        + "\n    {pn} rank default: reset về mặc định"
-        + "\n—————"
-        + "\n   {pn} [viewrole|view|show]: xem role của những lệnh đã chỉnh sửa",
+          + "\n   Với:"
+          + "\n   + <commandName>: tên lệnh"
+          + "\n   + <new role>: role mới của lệnh với:"
+          + "\n   + <new role> = 0: lệnh có thể được sử dụng bởi mọi thành viên trong nhóm"
+          + "\n   + <new role> = 1: lệnh chỉ có thể được sử dụng bởi quản trị viên"
+          + "\n   + <new role> = default: reset role lệnh về mặc định"
+          + "\n   Ví dụ:"
+          + "\n    {pn} rank 1: (lệnh rank sẽ chỉ có thể được sử dụng bởi quản trị viên)"
+          + "\n    {pn} rank 0: (lệnh rank sẽ có thể được sử dụng bởi mọi thành viên trong nhóm)"
+          + "\n    {pn} rank default: reset về mặc định"
+          + "\n—————"
+          + "\n   {pn} [viewrole|view|show]: xem role của những lệnh đã chỉnh sửa",
       en: "   {pn} <إسم الأمر> <القاعدة الجديدة>: قم بضبط القاعدة للأمر"
-        + "\n   مع:"
-        + "\n   + <إسم الأمر>: إسم الأمر"
-        + "\n   + <القاعدة الجديدة>: القاعدة الجديدة للأمر:"
-        + "\n   + <القاعدة الجديدة> = 0: يمكن للجميع إستخدام الأمر "
-        + "\n   + <قاعدة جديدة> = 1: يمكن إستخدام الامر فقط من طرف الآدمنية"
-        + "\n   + <الأمر الجديد > = الاصل : إستعادة قاعدة الأمر إلى 0"
-        + "\n   مثلل :"
-        + "\n    {pn} مستواي 1: (الأمر مستواي يمكن إستخدامه من طرف الآدمنية فقط)"
-        + "\n    {pn} مستواي 0: (يمكن إستخدام الأمر من طرف الجميع)"
-        + "\n    {pn} مستواي الأصل: إستعادة الأمر إلى ما كان عليه في البداية"
-        + "\n—————"
-        + "\n   {pn} [عرض القاعدة|عرض|رؤية]: مشاهدة قاعدة الأمر"
+          + "\n   مع:"
+          + "\n   + <إسم الأمر>: إسم الأمر"
+          + "\n   + <القاعدة الجديدة>: القاعدة الجديدة للأمر:"
+          + "\n   + <القاعدة الجديدة> = 0: يمكن للجميع إستخدام الأمر"
+          + "\n   + <قاعدة جديدة> = 1: يمكن إستخدام الامر فقط من طرف الآدمنية"
+          + "\n   + <الأمر الجديد > = الاصل : إستعادة قاعدة الأمر إلى 0"
+          + "\n   مثل:"
+          + "\n    {pn} مستواي 1: (الأمر مستواي يمكن إستخدامه من طرف الآدمنية فقط)"
+          + "\n    {pn} مستواي 0: (يمكن إستخدام الأمر من طرف الجميع)"
+          + "\n    {pn} مستواي الأصل: إستعادة الأمر إلى ما كان عليه في البداية"
+          + "\n—————"
+          + "\n   {pn} [عرض القاعدة|عرض|رؤية]: مشاهدة قاعدة الأمر"
     }
   },
 
@@ -54,25 +54,22 @@ module.exports = {
       noEditedCommand: "⚠️ | ليس هناك أي قاعدة لهذا الأمر تم تعديلها من قبل",
       editedCommand: "⚠️ مجموعتك لم يتم فيها اي تعديلات بالنسبة للقواعد:\n",
       noPermission: "❗ | فقط آدمنية المجموعة يمكنهم إستخدام هذا الأمر",
-      commandNotFound: " ❗ |الأمر  \"%1\" لم يتم إيجاده",
+      commandNotFound: " ❗ |الأمر \"%1\" لم يتم إيجاده",
       noChangeRole: "❗ | لم يتم إيجاد أي أمر تم التعديل عليه من قبل \"%1\"",
-      resetRole: " 🧿 | تم إعادة قاعدة الأمر  \"%1\" إلى الأصل",
-      changedRole: " ✅ |تم تغيير قاعدة الأمر  \"%1\" إلى  %2 بنجاح"
+      resetRole: " 🧿 | تم إعادة قاعدة الأمر \"%1\" إلى الأصل",
+      changedRole: " ✅ |تم تغيير قاعدة الأمر \"%1\" إلى %2 بنجاح"
     }
   },
 
-  onStart: async function ({ message, event, args, role, threadsData, getLang }) {
+  onStart: async function ({ message, event, args, role, threadsData, getLang, api }) {
     const { commands, aliases } = global.GoatBot;
     const setRole = await threadsData.get(event.threadID, "data.setRole", {});
 
     if (["view", "viewrole", "show"].includes(args[0])) {
       if (!setRole || Object.keys(setRole).length === 0)
         return message.reply(getLang("noEditedCommand"));
-
       let msg = getLang("editedCommand");
-      for (const cmd in setRole) {
-        msg += `- ${cmd} => ${setRole[cmd]}\n`;
-      }
+      for (const cmd in setRole) msg += `- ${cmd} => ${setRole[cmd]}\n`;
       return message.reply(msg);
     }
 
@@ -80,14 +77,12 @@ module.exports = {
     let newRole = args[1];
     if (!commandName || (isNaN(newRole) && newRole !== "default"))
       return message.SyntaxError();
-    
     if (role < 1)
       return message.reply(getLang("noPermission"));
 
     const command = commands.get(commandName) || commands.get(aliases.get(commandName));
     if (!command)
       return message.reply(getLang("commandNotFound", commandName));
-
     commandName = command.config.name;
     if (command.config.role > 1)
       return message.reply(getLang("noChangeRole", commandName));
@@ -101,11 +96,13 @@ module.exports = {
     }
 
     setRole[commandName] = newRole;
-    if (Default) {
+    if (Default)
       delete setRole[commandName];
-    }
+
+    // تأكيد على التنفيذ
+    await api.setMessageReaction("✅", event.messageID, (err) => {}, true);
 
     await threadsData.set(event.threadID, setRole, "data.setRole");
-    await message.reply("✅ " + (Default === true ? getLang("resetRole", commandName) : getLang("changedRole", commandName, newRole)));
+    message.reply("✅ " + (Default === true ? getLang("resetRole", commandName) : getLang("changedRole", commandName, newRole)));
   }
 };
